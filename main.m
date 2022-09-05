7fe0c49a7f422a6d2bac6501e216afa34b24779b
Jeronimo <andres.escobar9187@soyuco.edu.co>
2022-09-05 17:42:56 -0500

    Añadiendo codigo base

---
+++ main.m
@@ -0,0 +1,12 @@
+global mapa;
+global poblacionDepredadores;
+global poblacionPresas;
+global tasaReproduccionPresas;
+global tasaReproduccionDepredadores;
+
+mapa=zeros(500,500);
+poblacionDepredadores=10;
+poblacionPresas=50;
+tasaReproduccionPresas=1;
+tasaReproduccionDepredadores=1;
+
