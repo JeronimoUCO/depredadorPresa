05a03ca2d69fe3415a1b4d2ad779d601f53b01c1
Julián Vásquez <julidoo06@gmail.com>
2022-09-05 18:22:06 -0500

    Version local

--- main.m
+++ main.m
@@ -4,9 +4,43 @@ global poblacionPresas;
 global tasaReproduccionPresas;
 global tasaReproduccionDepredadores;
 
-mapa=zeros(500,500);
-poblacionDepredadores=10;
-poblacionPresas=50;
-tasaReproduccionPresas=1;
-tasaReproduccionDepredadores=1;
+
+depredadores = struct();
+
+mapa = zeros(500,500);
+poblacionDepredadores = 1;
+poblacionPresas = 1;
+tasaReproduccionPresas = 1;
+tasaReproduccionDepredadores = 1;
+
+function crearDepredador()
+
+  global poblacionDepredadores;
+  global depredadores;
+  depredadores(poblacionDepredadores).tiempoVidaSinComida = 10;
+  depredadores(poblacionDepredadores).posicionx = int64(rand()*500 + 1);
+  depredadores(poblacionDepredadores).posiciony = int64(rand()*500 + 1);
+  depredadores(poblacionDepredadores).direccionMovimiento = 1;
+  poblacionDepredadores += 1;
+
+endfunction
+
+
+
+function dibujarPantalla(instante)
+
+  global depredadores;
+  global poblacionDepredadores;
+
+  for i = 1:columns(depredadores)
+    depredadorActual = depredadores(i);
+
+
+
+
+
+
+  endfor
+endfunction
+
 
